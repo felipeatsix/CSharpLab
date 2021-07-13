bf1a9271f67a4cc2eff8ee6e8e77cbb8e2e6c53f
Felipe De Souza Santos <felipe-santos@outlook.com>
2021-07-13 12:02:34 +0200

    initial commit

---
+++ CSharpLab/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Human
+{
+    public class Human
+    {
+        public Human(DateTime birthday)
+        {
+            Birthday = birthday;
+        }
+
+        public DateTime Birthday { get; protected set; }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Now - Birthday;
+                return timeSpan.Days / 365;
+            }
+        }
+    }
+}
---
+++ CSharpLab/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var felipe = new Human(new DateTime(1989, 03, 27));
+            Console.WriteLine(felipe.Age);
+        }
+    }
+}
