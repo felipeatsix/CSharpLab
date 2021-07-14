1d5c971611bd3efaf5860524e122384be9765c2d
Felipe De Souza Santos <felipe-santos@outlook.com>
2021-07-14 20:53:39 +0200

    BasicClass

---
+++ BasicClass/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BasicClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var felipe = new Human(new DateTime(1989, 03, 27));
+            Console.WriteLine(felipe.Age);
+        }
+    }
+}
