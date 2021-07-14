330c2bb820fc11a89ebaf1cce187aa0dcea71e1d
Felipe De Souza Santos <felipe-santos@outlook.com>
2021-07-14 21:24:34 +0200

    Indexers

---
+++ Indexers/HttpCookie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        private readonly Dictionary<string, string> _dictionary;
+
+        public HttpCookie(Dictionary<string, string> dictionary)
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        // Declare indexer
+
+        public string this[string key]
+        {
+            get => _dictionary[key];
+            set => _dictionary[key] = value;
+        }
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, string>();
+            dictionary["name"] = "Felipe";
+
+            // The HttpCookie must have an indexer so we can pass new key/value pairs to it like we do for dictionary.
+            var cookie = new HttpCookie(new Dictionary<string, string>());
+            cookie["name"] = "Felipe";
+            cookie["last name"] = "Santos";
+            Console.WriteLine("{0} {1}", cookie["name"], cookie["last name"]);
+            Console.WriteLine(dictionary["name"]);
+        }
+    }
+}
