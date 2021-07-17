6c8e8aed7c628a10e6caf0cd10ec6dbabaf8fcbc
Felipe De Souza Santos <felipe-santos@outlook.com>
2021-07-17 15:48:29 +0200

    small update

--- Events/SlackService.cs
+++ Events/SlackService.cs
@@ -6,7 +6,7 @@ namespace Events
     {
         public void OnDocumentationSent(object source, DocumentationPublishedHandler e)
         {
-            Console.WriteLine("[Slack]: New documentation has been published: {0}", e.Document.Title);
+            Console.WriteLine("[Slack]: New documentation has been published: {0} ({1}...)", e.Document.Title, e.Document.Text[0..10]);
         }
     }
 }
