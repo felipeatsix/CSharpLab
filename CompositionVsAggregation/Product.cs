31663a53be05ceb09343428bf1ef26683e4d6b34
fdesouzasantos <fdesouzasantos@wayfair.com>
2022-04-25 14:36:56 +0200

    New project

---
+++ CompositionVsAggregation/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CompositionVsAggregation
+{
+    public class Product
+        // This class will be CONTAINED by the vending machine - Aggregation
+    {
+        private readonly string _name;
+        private readonly double _price;
+
+        public Product(string name, double price)
+        {
+            _name = name;
+            _price = price;
+        }
+
+        // Provides a public functionality (method) to show product details on screen
+        // We use "public" access modifier because Product could be used in other classes and other assemblies.
+        public void PrintDetails()
+        {
+            Console.WriteLine($"[{_name} : ${_price}]");
+        }
+    }
+}
